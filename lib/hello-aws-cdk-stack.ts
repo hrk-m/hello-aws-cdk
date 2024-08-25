89b7aea92af823812cc6a50de52931daf3777466
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-08-25 17:58:38 +0900

    APIゲートウェイとlambdaを使いREST API /hello を作成

--- lib/hello-aws-cdk-stack.ts
+++ lib/hello-aws-cdk-stack.ts
@@ -1,16 +1,30 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import Lambda L2 construct
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+//Import API Gateway L2 construct
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 
 export class HelloAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the Lambda function resource
+    // Lambda 関数リソースを作成
+    const helloWorldFunction = new lambda.Function(this, 'HelloWorldFunction', {
+      runtime: lambda.Runtime.NODEJS_20_X, // 関数が実行される環境。ここでは、Node.jsバージョン を使用します20.x。
+      code: lambda.Code.fromAsset('lambda'), // ローカルマシン上の関数コードへのパス。
+      handler: 'hello.handler', // 関数コードを含む特定のファイルの名前。
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloAwsCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the API Gateway resource
+    const api = new apigateway.LambdaRestApi(this, 'HelloWorldApi', {
+      handler: helloWorldFunction,
+      proxy: false,
+    });
+
+    // Define the '/hello' resource with a GET method
+    const helloResource = api.root.addResource('hello');
+    helloResource.addMethod('GET');
   }
 }
