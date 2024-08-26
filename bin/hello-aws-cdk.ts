71aa6ff96828b40850602ae7e4a582a086c1b8e3
Haruki Miyagi <haruki.miyagi11@gmail.com>
2024-08-26 10:53:05 +0900

    region 変更

--- bin/hello-aws-cdk.ts
+++ bin/hello-aws-cdk.ts
@@ -1,6 +1,6 @@
 #!/opt/homebrew/opt/node/bin/node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
 import { HelloAwsCdkStack } from '../lib/hello-aws-cdk-stack';
 
 const app = new cdk.App();
@@ -15,7 +15,7 @@ new HelloAwsCdkStack(app, 'HelloAwsCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { region: 'ap-northeast-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
